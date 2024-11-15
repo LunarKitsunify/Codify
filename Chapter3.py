0cc1704ea67e401d8f849348a800f3b8c6eecb63
Kitsunify <ivankitsunify@gmail.com>
2024-11-15 18:29:57 +0600

    Add Chapter 3

---
+++ Chapter3.py
@@ -0,0 +1,72 @@
+#region Task 1
+
+# name = "Ivan"
+# print(f"Hello, {name}!!!")
+
+#endregion
+
+#region Task 2
+
+# num1 = 5
+# num2 = 7
+
+# print(f"{num1+num2}")
+# print(f"{num1*num2}")
+# print(f"{num1/num2}")
+# print(f"{num1**num2}")
+
+#endregion
+
+#region Task 3
+
+# valueInt = int(input("Введите число: "))
+# valueString = input("Введите текст: ")
+# outputText = ""
+
+# while valueInt != 0:
+#         outputText += valueString
+#         valueInt -= 1
+
+# print(outputText)
+
+#endregion
+
+#region Task 4
+
+# print("Step1")
+# print("Step2")
+# print("Step3")
+
+#endregion
+
+#region Task 5
+
+# num1 = int(input("Введите первое число: "))
+# num2 = int(input("Введите второе число: "))
+
+# print(f"{num1+num2}")
+# print(f"{num1-num2}")
+# print(f"{num1*num2}")
+# print(f"{num1/num2}")
+
+#endregion
+
+#region Task 6
+
+# num1 = int(input("Введите первое число: "))
+# num2 = int(input("Введите второе число: "))
+# num3 = int(input("Введите третье число: "))
+
+# print(f"{(num1+num2+num3)/3}")
+
+
+#endregion
+
+#region Task 7
+
+# Напишите программу, которая запрашивает у пользователя строку, представляющую число, и преобразует её в целое число и число с плавающей запятой.
+
+# valueInt = int(input("Введите число: "))
+# print(f"{float(valueInt)}")
+
+#endregion 
